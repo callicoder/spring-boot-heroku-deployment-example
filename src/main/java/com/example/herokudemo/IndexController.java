62752c2ec44af0276ae91d5aca6ae03cff22e652
Rajeev Kumar Singh <rajeevs@flock.co>
2018-04-01 14:40:25 +0530

    initial commit

---
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.herokudemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello there! I'm deployed successfully";
+    }
+}
---
+++ src/test/java/com/example/herokudemo/HerokuDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.herokudemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokuDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
