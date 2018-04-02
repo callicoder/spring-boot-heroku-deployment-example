23501b4d44a060237f97e1d8c4db72cd2477cbcc
Rajeev Kumar Singh <rajeevs@flock.co>
2018-04-02 15:16:03 +0530

    Response message

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello there! I'm deployed successfully";
+        return "Hello there! I'm running.";
     }
 }
